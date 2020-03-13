dc0bf98275b7f77c3dde3512b71c9c38d79f2abd
St4ri0nS <62130639+Starions@users.noreply.github.com>
2020-03-13 23:47:20 +0700

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,60 @@
+clear
+sleep 2
+echo " \033[1;33m   Author :\033[1;32m Mr.St4ri0ns\033[1;34m |\033[1;33m Create\033[1;32m 24 maret    "
+echo "\033[1;32m<\033[1;31m════════════════════════════════════════════\033[1;32m>"
+echo " \033[1;35m      [\033[1;32m+\033[1;35m]\033[1;37m  STARIVER CYBER NETWORK \033[1;35m [\033[1;32m+\033[1;35m] "                        >
+echo ""
+echo "\033[1;35m[\033[1;32m•\033[1;35m]\033[1;37m LIST TOOLS VIRTEX\033[1;35m [\033[1;32m•\033[1;35m]"
+echo ""
+echo "\033[1;35m[\033[1;33m1\033[1;35m]\033[1;37m VIRTEX\033[1;31m 1"
+echo "\033[1;35m[\033[1;33m2\033[1;35m]\033[1;37m VIRTEX\033[1;31m 2"
+echo "\033[1;35m[\033[1;33m3\033[1;35m]\033[1;37m VIRTEX\033[1;31m 3"
+echo "\033[1;35m[\033[1;33m4\033[1;35m]\033[1;37m VIRTEX\033[1;31m 4"
+echo ""
+read -p "   SELECT YOUR INPUT :" input
+
+if [ $input = "1" ]
+then
+        echo "\033[1;32mPLEASE WAIT..."
+        sleep 3
+        echo "\033[1;31m[-] GENERATE LINK VIRTEX"
+        sleep 5
+        echo "\033[1;37m[+] LINK ()"
+        sleep 2
+        echo ""
+        echo "\033[1;37m[-] COPY LINK DI ATAS TANPA TANDA KURUNG LALU TEMPELKAN DI CROME"
+
+elif [ $input = "2" ]
+then
+        echo "\033[1;32mPLEASE WAIT..."
+        sleep 3
+        echo "\033[1;31m[-] GENERATE LINK VIRTEX"
+        sleep 5
+        echo "\033[1;37m[+] LINK ()"
+        sleep 2
+        echo ""
+        echo "\033[1;37m[-] COPY LINK DI ATAS TANPA TANDA KURUNG LALU TEMPELKAN DI CROME"
+
+elif [ $input = "3" ]
+then
+        echo "\033[1;32mPLEASE WAIT..."
+        sleep 3
+        echo "\033[1;31m[-] GENERATE LINK VIRTEX"
+        sleep 5
+        echo "\033[1;37m[+] LINK ()"
+        sleep 2
+        echo ""
+        echo "\033[1;37m[-] COPY LINK DI ATAS TANPA TANDA KURUNG LALU TEMPELKAN DI CROME"
+
+elif [ $input = "4" ]
+then
+        echo "\033[1;32mPLEASE WAIT..."
+        sleep 3
+        echo "\033[1;31m[-] GENERATE LINK VIRTEX"
+        sleep 5
+        echo "\033[1;37m[+] LINK ()"
+        sleep 2
+        echo ""
+        echo "\033[1;37m[-] COPY LINK DI ATAS TANPA TANDA KURUNG LALU TEMPELKAN DI CROME"
+
+fi
